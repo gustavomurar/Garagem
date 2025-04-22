f9b03a2d27cdbc63a9c8394fe47a8c8efbb1e32a
gustavomurar <gustavomurarosoares3@gmail.com>
2025-04-22 09:22:39 -0300

    feat: criação da API para Cor

--- core/models/acessorio.py
+++ core/models/acessorio.py
@@ -4,4 +4,4 @@ class Acessorio(models.Model):
     descricao = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
-        return f"({self.id})"
+        return f"({self.id}){self.descricao.upper()}"
