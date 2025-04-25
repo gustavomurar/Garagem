072cdee7bd8324ff5ea6cf509c89e2a4fd6b4ee1
gustavomurar <gustavomurarosoares3@gmail.com>
2025-04-25 15:10:33 -0300

    feat: inclusão do relacionamento de Veiculo com Modelo e Cor

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -1,6 +1,10 @@
 from django.db import models
+from .cor import Cor
+from .modelo import Modelo
 
 class Veiculo(models.Model):
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo", null=True, blank=True)
     ano = models.IntegerField(null=True, default=0)
     preco = models.DecimalField(max_digits=10,decimal_places=2, null=True, default=0)
 
